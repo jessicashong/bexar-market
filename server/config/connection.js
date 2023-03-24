ba1d95934e7546a4cff84fe7e637519be050363a
mdinkelbach <113461649+mdinkelbach@users.noreply.github.com>
2023-03-23 21:34:24 -0500

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bexarmarket',
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bexar-market',
         {
             useNewUrlParser: true,
             useUnifiedTopology: true,
