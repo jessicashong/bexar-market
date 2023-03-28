6c97e4e181168014c0e0a528d43997300a31cf23
Todey <chloe.e.hanks@gmail.com>
2023-03-28 18:45:25 -0500

    added comments about functionality concerning the modal, state, and event handling; added TODOs to connect with backend

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -5,9 +5,10 @@ import React, { useState } from 'react';
 // import { Button } from "@material-tailwind/react";
 import Modal from "../ModalSignup/index.js";
 
-
+// TODO: update this with backend
 var categories = ['leather', 'woodworking', 'jewelry', 'textiles'];
 
+// TODO: handle any backend logic between users, categories, etc. 
 
 // TODO: handle checkbox event
 // function Checkbox() { 
@@ -24,21 +25,23 @@ var categories = ['leather', 'woodworking', 'jewelry', 'textiles'];
 // }; 
 
 // TODO: change input divs to Form (based on Book Search Signup.js)
+// TODO: 
 function Signup() {
-  const text = 'some text';
-  const [showModal, setShowModal] = useState(false);
 
-  return (
-  <>
+    // create state to show modal and to update modal; initialize to false (modal will not show until button is clicked)
+    const [showModal, setShowModal] = useState(false);
 
-  <p className='bg-orange-400' >Hello World! Here is {text}</p>
-  
-  <div className="bg-grey-lighter min-h-screen flex flex-col">
+    return (
+    <>
+
+    <p className='bg-orange-400' >Hello World! Here is {text}</p>
+
+    <div className="bg-grey-lighter min-h-screen flex flex-col">
             <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
                 <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
                     <h1 className="mb-8 text-3xl text-center">Sign up</h1>
                     <div className='flex flex-col border border-grey-light w-full p-3 rounded mb-4'>
-                      {/* change all these inputs to Form.Group ; see booksearch SignupForm.js */}
+                        {/* change all these inputs to Form.Group ; see booksearch SignupForm.js */}
                     <input 
                         type="text"
                         className="block border border-grey-light w-full p-3 rounded mb-4"
@@ -63,29 +66,29 @@ function Signup() {
                         placeholder="Confirm Password" />
                     {/* loops through the categories array to create checkbox */}
                     <div className='block border border-grey-light w-full p-3 rounded mb-4'>
-                      <h3 className="italic mb-4">What categories are you interested in?</h3>
-                      {categories.map((categoryName, i) => {
-                          return (
+                        <h3 className="italic mb-4">What categories are you interested in?</h3>
+                        {categories.map((categoryName, i) => {
+                            return (
                                 // add key here in case this specific item changes, then jsx knows to only change that    specific element
                                 // make sure to use the category id from the typeDef(?)
                                 <div key={i}>
-                                  <input
-                                  className=" flex-row dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary"
-                                  type="checkbox"
-                                  value=""
-                                  id="checkboxDefault" 
-                                  
-                                  />
-                                  <label
-                                  className="flex-row pl-[0.15rem] hover:cursor-pointer"
-                                  htmlFor="checkboxDefault"
-                                  
-                                  >
-                                  {categoryName}
-                                  </label>
+                                    <input
+                                    className=" flex-row dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary"
+                                    type="checkbox"
+                                    value=""
+                                    id="checkboxDefault" 
+                                    
+                                    />
+                                    <label
+                                    className="flex-row pl-[0.15rem] hover:cursor-pointer"
+                                    htmlFor="checkboxDefault"
+                                    
+                                    >
+                                    {categoryName}
+                                    </label>
                                 </div> 
-                          );
-                      })}
+                            );
+                        })}
                     </div>
                     </div>
                     
@@ -97,6 +100,7 @@ function Signup() {
                     <button
                         type="submit"
                         className="bg-orange-400 w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
+                        // when this button is clicked, set the modal to be true (modal appears)
                         onClick ={ () => setShowModal(true)}
                     >Show Modal</button>
                     </div>
@@ -110,9 +114,10 @@ function Signup() {
                 </div>
             </div>
         </div>
+        {/* set isVisible to the showModal state and set showModal=false when modal is closed */}
         <Modal isVisible ={showModal} onClose={() => setShowModal(false)}/>
-  </>
-  );
+    </>
+    );
 }
 
 export default Signup;
