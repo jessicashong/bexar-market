16a5fc963fa03f8c0a7fc97f2fc08256986a60c7
Todey <chloe.e.hanks@gmail.com>
2023-03-28 18:47:08 -0500

    removed <p> tag from the top of Signup()'s return function; this was a carry over from my sandbox code

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -34,8 +34,6 @@ function Signup() {
     return (
     <>
 
-    <p className='bg-orange-400' >Hello World! Here is {text}</p>
-
     <div className="bg-grey-lighter min-h-screen flex flex-col">
             <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
                 <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
