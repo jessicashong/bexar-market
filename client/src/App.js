ca1eaaf050737edeb2ceb4d411222218180eaab7
mdinkelbach <mj.dinkelbach@mgail.com>
2023-03-27 19:10:39 -0500

    added page imports

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,12 @@ import { setContext } from '@apollo/client/link/context';
 import './index.css';
 import Nav from './components/Nav';
 import Home from './pages/Home';
+import About from './pages/About';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Search from './pages/Search';
+import Signup from './pages/Signup';
+import Store from './pages/Store';
 
 const httpLink = createHttpLink({
     uri: '/graphql',
