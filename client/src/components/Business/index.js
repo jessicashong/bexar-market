8f0caeb5de13512b4a8f523337a4ac330b2e30cc
mdinkelbach <mj.dinkelbach@mgail.com>
2023-03-27 19:49:36 -0500

    setup display for each business on home page

--- client/src/components/Business/index.js
+++ client/src/components/Business/index.js
@@ -4,3 +4,31 @@ import { Link } from "react-router-dom";
 // Import style.css to include tailwind directives
 import './style.css';
 
+function Business(business) {
+
+    const {
+        image,
+        name,
+        _id,
+        catagory,
+        description
+      } = business;
+
+    return (
+        <div className="card px-1 py-1">
+          <Link to={`/businesses/${_id}`}>
+            <img
+              alt={name}
+              src={`/images/${image}`}
+            />
+            <p>{name}</p>
+          </Link>
+          <div>
+            <span>Catagory: {catagory}</span>
+            <div>{description}</div>
+          </div>
+        </div>
+      );
+}
+
+export default Business;
