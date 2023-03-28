8ba4ff31dbb2d6e440cdd2dc5a9de3d4b1af72d1
jessicashong <jessicashong12@gmail.com>
2023-03-28 18:21:08 -0500

    udpated resolvers with mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -36,13 +36,21 @@ const resolvers = {
             }
             throw new AuthenticationError('You need to be logged in.');
         },
-        addProduct: ,
+        addProduct: async (parent, { product }, context) => {
+            console.log(context);
+            if (context.business) {
+                const product = await Product.create({ product });
+                return { product };
+            }
+            throw new AuthenticationError('You must be logged in as a business.')
+        } ,
         updateProduct: async (parent, args, context) => {
             if(context.product){
                 return Product.findByIdAndUpdate(context.product.productId, args, {
                     new: true,
                 });
             }
+            throw new AuthenticationError('You must be logged in as a business.')
         },
         deleteProduct: async (parent, args, context) => {
             if(context.product){
@@ -50,11 +58,42 @@ const resolvers = {
                     new: true,
                 });
             }
+            throw new AuthenticationError('You must be logged in as a business.')
         },
-        addBusiness: ,
-        updateBusiness: ,
-        addFavorite: ,
-        deleteFavorite: ,
+        addBusiness: async (parent, args, context) => {
+            const business = await Business.create(args);
+            const token = signToken(business);
+            return { token, business };
+        }, 
+        updateBusiness: async (parent, args, context) => {
+            if (context.business) {
+                return await Business.findByIdAndUpdate(context.business._id, args, { new: true });
+            }
+            throw new AuthenticationError('You must be logged in as a business.');
+        },
+        addFavorite: async (parent, { product }, context) => {
+            console.log(context);
+            if (context.user){
+                const favorites = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { favorites: product } },
+                    { new: true }
+                )
+                return favorites;
+            }
+            throw new AuthenticationError('You must be logged in.');
+        },
+        deleteFavorite: async (parent, { productId }, context) => {
+            if (context.user) {
+                const favorites = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { favorites: { productId: productId } } },
+                    { new: true }
+                );
+                return favorites;
+            }
+            throw new AuthenticationError('You must be logged in.');
+        },
         login: async(parent, { email, password }) => {
             const user = await User.findOne({ email });
 
