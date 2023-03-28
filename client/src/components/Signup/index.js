583c43ee50f07193047515bf74a30f1ba6d51c6a
Todey <chloe.e.hanks@gmail.com>
2023-03-28 18:37:39 -0500

    developed initial signup page for user; need to split signup into two columns: one for vendor, one for shopper; then develop more

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -1,2 +1,118 @@
 // Import style.css to include tailwind directives
 import './style.css';
+
+import React, { useState } from 'react';
+// import { Button } from "@material-tailwind/react";
+import Modal from "../ModalSignup/index.js";
+
+
+var categories = ['leather', 'woodworking', 'jewelry', 'textiles'];
+
+
+// TODO: handle checkbox event
+// function Checkbox() { 
+//   const handleChange = () => { 
+//     console.log('The checkbox was toggled'); 
+//   }; 
+  
+//   return ( 
+//     <div>  
+//       <input type="checkbox" onChange={handleChange}
+//       />
+//     </div> 
+//   ); 
+// }; 
+
+// TODO: change input divs to Form (based on Book Search Signup.js)
+function Signup() {
+  const text = 'some text';
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+  <>
+
+  <p className='bg-orange-400' >Hello World! Here is {text}</p>
+  
+  <div className="bg-grey-lighter min-h-screen flex flex-col">
+            <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+                <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+                    <h1 className="mb-8 text-3xl text-center">Sign up</h1>
+                    <div className='flex flex-col border border-grey-light w-full p-3 rounded mb-4'>
+                      {/* change all these inputs to Form.Group ; see booksearch SignupForm.js */}
+                    <input 
+                        type="text"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="fullname"
+                        placeholder="Full Name" />
+
+                    <input 
+                        type="text"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="email"
+                        placeholder="Email" />
+
+                    <input 
+                        type="text"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="password"
+                        placeholder="Password" />
+                    <input 
+                        type="text"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="confirm_password"
+                        placeholder="Confirm Password" />
+                    {/* loops through the categories array to create checkbox */}
+                    <div className='block border border-grey-light w-full p-3 rounded mb-4'>
+                      <h3 className="italic mb-4">What categories are you interested in?</h3>
+                      {categories.map((categoryName, i) => {
+                          return (
+                                // add key here in case this specific item changes, then jsx knows to only change that    specific element
+                                // make sure to use the category id from the typeDef(?)
+                                <div key={i}>
+                                  <input
+                                  className=" flex-row dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary"
+                                  type="checkbox"
+                                  value=""
+                                  id="checkboxDefault" 
+                                  
+                                  />
+                                  <label
+                                  className="flex-row pl-[0.15rem] hover:cursor-pointer"
+                                  htmlFor="checkboxDefault"
+                                  
+                                  >
+                                  {categoryName}
+                                  </label>
+                                </div> 
+                          );
+                      })}
+                    </div>
+                    </div>
+                    
+                    <div>
+                    <button
+                        type="submit"
+                        className="bg-orange-400 w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
+                    >Create Account</button>
+                    <button
+                        type="submit"
+                        className="bg-orange-400 w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
+                        onClick ={ () => setShowModal(true)}
+                    >Show Modal</button>
+                    </div>
+                </div>
+
+                <div className="text-grey-dark mt-6">
+                    Already have an account? 
+                    <a className="no-underline border-b border-blue text-blue" href="../login/">
+                        Log in
+                    </a>.
+                </div>
+            </div>
+        </div>
+        <Modal isVisible ={showModal} onClose={() => setShowModal(false)}/>
+  </>
+  );
+}
+
+export default Signup;
