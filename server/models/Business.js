424ee5b1a503a4007c3a31599bdb30de3d2b15c5
LDudrey <113869466+LDudrey@users.noreply.github.com>
2023-03-28 21:25:27 -0500

    added auth to business model

--- server/models/Business.js
+++ server/models/Business.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Product = require('./Product');
 
@@ -31,6 +32,21 @@ const businessSchema = new Schema({
   },
   products: [Product.schema]
 });
+
+// set up pre-save middleware to create password
+businessSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+businessSchema.methods.isCorrectPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 const Business = model('Business', businessSchema);
 
