64e07f46a003b0ddf9dac5ebcd4ff3306f1a5615
mdinkelbach <mj.dinkelbach@mgail.com>
2023-03-28 19:23:23 -0500

    created framework for business profile product list items

--- client/src/components/BusinessProductList/index.js
+++ client/src/components/BusinessProductList/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Auth from "../../utils/auth";
-import { Link } from "react-router-dom";
 import BusinessProductItem from "../BusinessProductItem"
 // Import style.css to include tailwind directives
 import './style.css';
@@ -10,16 +8,17 @@ function BusinessProductList() {
     return (
         <div>
             <h2>Your Favorited Items</h2>
-            {state.favorites.length ? (
+            {state.products.length ? (
                 <div className="flex-row">
-                    {filterfavorites().map((favorite) => (
+                    {filterfavorites().map((product) => (
                         <BusinessProductItem
-                            key={favorite._id}
-                            _id={favorite._id}
-                            image={favorite.image}
-                            name={favorite.businessName}
-                            price={favorite.price}
-                            description={favorite.description}
+                            key={product._id}
+                            _id={product._id}
+                            image={product.image}
+                            name={product.name}
+                            price={product.price}
+                            description={product.description}
+                            quantity={product.quantity}
                         />
                     ))}
                 </div>
