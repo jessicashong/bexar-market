8b9a2b08e55bec4c3371c956bbc26e63206cf2da
mdinkelbach <mj.dinkelbach@mgail.com>
2023-03-28 19:02:01 -0500

    merge

--- client/src/components/BusinessProductList/index.js
+++ client/src/components/BusinessProductList/index.js
@@ -1,2 +1,17 @@
+import React from 'react';
+import Auth from "../../utils/auth";
+import { Link } from "react-router-dom";
 // Import style.css to include tailwind directives
 import './style.css';
+
+function BusinessProductList() {
+
+
+    return (
+        <div>
+            <h2></h2>
+        </div>
+    )
+}
+
+export default BusinessProductList;
