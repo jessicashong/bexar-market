d9739f5ebfbfc17746f2a654ee41ede4aa3432fb
Todey <chloe.e.hanks@gmail.com>
2023-03-30 09:43:39 -0500

    created two forms: USER and BUSINESS in Signup > index.js

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -35,9 +35,17 @@ function Signup() {
     <>
 
     <div className="bg-grey-lighter min-h-screen flex flex-col">
+        <div className="mx-auto flex-1 flex flex-row items-center justify-center px-2 text-grey-dark mt-6 bg-white px-6 py-8 rounded shadow-md text-black w-full border border-red-700">
+                Already have an account?  
+                <a className="no-underline border-b border-blue text-blue" href="../login/">
+                    Log in
+                </a>.
+        </div>
             <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-                <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-                    <h1 className="mb-8 text-3xl text-center">Sign up</h1>
+
+                {/* USER SIGNUP */}
+                <form className="bg-white px-6 py-8 rounded shadow-md text-black w-full border border-red-700">
+                    <h1 className="mb-8 text-3xl text-center">Create User Account</h1>
                     <div className='flex flex-col border border-grey-light w-full p-3 rounded mb-4'>
                         {/* change all these inputs to Form.Group ; see booksearch SignupForm.js */}
                     <input 
@@ -94,7 +102,7 @@ function Signup() {
                     <button
                         type="submit"
                         className="bg-orange-400 w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
-                    >Create Account</button>
+                    >Submit User Account</button>
                     <button
                         type="submit"
                         className="bg-orange-400 w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
@@ -102,14 +110,83 @@ function Signup() {
                         onClick ={ () => setShowModal(true)}
                     >Show Modal</button>
                     </div>
-                </div>
+                </form>
+                
+                {/* BUSINESS SIGNUP */}
+                <form className="bg-white px-6 py-8 rounded shadow-md text-black w-full border border-red-700 m-5">
+                    <h1 className="mb-8 text-3xl text-center">Create Business Account</h1>
+                    <div className='flex flex-col border border-grey-light w-full p-3 rounded mb-4'>
+                        {/* change all these inputs to Form.Group ; see booksearch SignupForm.js */}
+                    <input 
+                        type="text"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="business-name"
+                        placeholder="Business Name" />
 
-                <div className="text-grey-dark mt-6">
-                    Already have an account? 
-                    <a className="no-underline border-b border-blue text-blue" href="../login/">
-                        Log in
-                    </a>.
-                </div>
+                    <input 
+                        type="text"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="email"
+                        placeholder="Business Email" />
+
+                    <input 
+                        type="text"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="password"
+                        placeholder="Password" />
+                    <input 
+                        type="text"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="confirm_password"
+                        placeholder="Confirm Password" />
+                    <input 
+                        type="file"
+                         />
+
+                    {/* loops through the categories array to create checkbox */}
+                    <div className='block border border-grey-light w-full p-3 rounded mb-4'>
+                      {/* TODO: MAKE THIS REQUIRED */}
+                        <h3 className="italic text-center mb-4">What categories do you sell?</h3>
+                        {categories.map((categoryName, i) => {
+                            return (
+                                // add key here in case this specific item changes, then jsx knows to only change that    specific element
+                                // make sure to use the category id from the typeDef(?)
+                                <div key={i}>
+                                    <input
+                                    className=" flex-row dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary"
+                                    type="checkbox"
+                                    value=""
+                                    id="checkboxDefault" 
+                                    
+                                    />
+                                    <label
+                                    className="flex-row pl-[0.15rem] hover:cursor-pointer"
+                                    htmlFor="checkboxDefault"
+                                    
+                                    >
+                                    {categoryName}
+                                    </label>
+                                </div> 
+                            );
+                        })}
+                    </div>
+                    </div>
+                    
+                    <div>
+                    <button
+                        type="submit"
+                        className="bg-orange-400 w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
+                    >Submit Business Account</button>
+                    {/* {mySignupChoice()} */}
+                    <button
+                        type="submit"
+                        className="bg-orange-400 w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
+                        // when this button is clicked, set the modal to be true (modal appears)
+                        onClick ={ () => setShowModal(true)}
+                    >Show Modal</button>
+                    </div>
+                </form>
+
             </div>
         </div>
         {/* set isVisible to the showModal state and set showModal=false when modal is closed */}
