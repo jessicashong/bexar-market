82a813698859b3264e928dadf846b205fb741e23
mdinkelbach <mj.dinkelbach@mgail.com>
2023-04-01 12:33:25 -0500

    profile page css complete

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 
 function About() {
 
@@ -10,41 +9,39 @@ function About() {
                 <p>Our mission at Bexar Market is to create an open space for small businesses to share their passion with the world. We wanted to showcase the amazing talent that can be found here in the heart of Bexar County, and allow those who cannot physically explore to see the collections that Bexar artists can offer. Please feel free to browse and even create your own account to add favorites and discover your new favorite creators.</p>
             </div>
             <div className='contributors'>
+                <h2>Contributors</h2>
                 <ul>
-                    <li className='profile-card'>
-                        <p>Chloe</p>
+                    <li className='portfolio-card'>
                         <a
                             href="https://chloeeh.github.io/hanks-react-portfolio/"
                         >
-                        <p>Portfolio Site</p>
+                        <p>Chloe Hanks</p>
                         </a>
                     </li>
-                    <li className='profile-card'>
-                        <p>Jessica</p>
+                    <li className='portfolio-card'>
                         <a
                             href="https://jessicashong.github.io/react-portfolio/"
                         >
-                        <p>Portfolio Site</p>
+                        <p>Jessica Hong</p>
                         </a>
                     </li>
-                    <li className='profile-card'>
-                        <p>Lydia</p>
+                    <li className='portfolio-card'>
                         <a
                             href="https://ldudrey.github.io/portfolio-v2/"
                         >
-                        <p>Portfolio Site</p>
+                        <p>Lydia Dudrey</p>
                         </a>
                     </li>
-                    <li className='profile-card'>
-                        <p>Michael</p>
+                    <li className='portfolio-card'>
                         <a
                             href="https://mdinkelbach.github.io/portfolio-resume/"
                         >
-                        <p>Portfolio Site</p>
+                        <p>Michael Dinkelbach</p>
                         </a>
                     </li>
                 </ul>
             </div>
+            
         </div>
     )
 }
