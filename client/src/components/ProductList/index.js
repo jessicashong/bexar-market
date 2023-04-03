d272af153908f61e4b5c657bf35103b7b2b611d8
mdinkelbach <mj.dinkelbach@mgail.com>
2023-04-03 16:28:26 -0500

    CSS for singular business page

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -3,6 +3,7 @@ import { useQuery } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 import ProductItem from "../ProductItem"
 import { QUERY_BUSINESS } from '../../utils/queries';
+import businessImage from "../../assets/supportSmallBusiness.jpg"
 
 //TODO: show products only for current business
 const ProductList = () => {
@@ -21,20 +22,24 @@ const ProductList = () => {
         return <h2>Loading...</h2>
     }
     return (
-        <div>
-            <div className="card px-1 py-1">
+        <div className='business'>
+            <div className="business-title">
+                <div>
                 <img
                     alt={business.businessName}
-                    src={`/images/${business.image}`}
+                    src={businessImage}
                 />
+                </div>
+                <div className='business-info'>
                 <p>{business.businessName}</p>
                 <div>
-                    <div>{business.description}</div>
+                    <div className='business-desc'>{business.description}</div>
                     <div>{business.categories}</div>
+                </div>
                 </div>
                 {/* <button onClick={addToCart}>Add to cart</button> */}
             </div>
-            <h2>Our Products</h2>
+            <h2 className='products'>Our Products</h2>
             <div className="flex-row">
                 {business.products.map((product) => (
                     <ProductItem
