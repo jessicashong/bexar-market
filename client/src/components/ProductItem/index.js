b14e1a5e19e7b019360993acdcd2939534e3c249
Chloe <114367776+chloeeh@users.noreply.github.com>
2023-04-03 16:58:18 -0500

    Update index.js
    added updates from chloe's code to incorporate static image

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import productImage from "../../assets/product.png"
 
 function ProductItem(product) {
 
@@ -12,10 +13,10 @@ function ProductItem(product) {
 
   return (
     <div className="card px-1 py-1">
-      {/*<img
+      <img
         alt={name}
-        src={image}
-      />*/}
+        src={productImage}
+      />
       <div className='product-info'>
         <p>{productName}</p>
         <div>{description}</div>
