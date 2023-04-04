6d74d0aff65947a9b8870e97729464fb170b7665
jessicashong <jessicashong12@gmail.com>
2023-04-04 00:34:15 -0500

    add favorite and delete favorite WORKSSSSS

--- client/src/components/UserFavoriteItem/index.js
+++ client/src/components/UserFavoriteItem/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { removeFaveId } from '../../utils/helpers';
+import { DELETE_FAVORITE } from '../../utils/mutations';
+import { QUERY_ME } from '../../utils/queries';
+import { useMutation, useQuery } from '@apollo/client';
+import Auth from '../../utils/auth';
+
 
 function UserFavoriteItem(product) {
 
@@ -11,6 +17,38 @@ function UserFavoriteItem(product) {
         description,
         quantity
       } = product;
+
+      const { loading, data } = useQuery(QUERY_ME);
+      const [deleteFavorite, { error }] = useMutation(DELETE_FAVORITE, {
+        update(cache, { data: { deleteFavorite } }) {
+            try{
+                cache.writeQuery({
+                query: QUERY_ME,
+                data: { me: deleteFavorite},
+                });
+            } catch(err) {
+                console.error(err);
+            }
+        },
+    })
+
+    const handleRemoveFave = async () => {
+      const token = Auth.loggedIn() ? Auth.getToken() : null;
+      console.log('productId:', _id)
+      if (!token) {
+      return false;
+      }
+
+      try {
+      const { data } = await deleteFavorite({
+          variables: { productId: _id },
+      });
+      // upon success, remove book's id from localStorage
+      removeFaveId(_id);
+      } catch (err) {
+      console.error(err);
+      }
+  };
 
     return (
         <div className="card px-1 py-1">
@@ -25,6 +63,16 @@ function UserFavoriteItem(product) {
             <span>Price: {price}</span>
             <span>{quantity} in stock</span>
             <div>{description}</div>
+            <button
+            type="button"
+            className="bg-orange-400 text-center p-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
+            onClick={ () => {
+              handleRemoveFave(_id);
+              }
+            }
+        >
+          Delete Favorite
+        </button>
           </div>
         </div>
     )
--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,4 +1,35 @@
 export function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
-  }
+  };
+
+  export const getFaveProductIds = () => {
+    const savedFaveProductIds = localStorage.getItem('savedFave')
+      ? JSON.parse(localStorage.getItem('savedFave'))
+      : [];
+
+      return savedFaveProductIds;
+  };
+
+  export const saveFaveIds = (productIdArr) => {
+    if (productIdArr.length) {
+      localStorage.setItem('savedFave', JSON.stringify(productIdArr));
+    } else {
+      localStorage.removeItem('savedFave');
+    }
+  };
+
+  export const removeFaveId = (productId) => {
+    const savedFaveProductIds = localStorage.getItem('savedFave')
+      ? JSON.parse(localStorage.getItem('savedFave'))
+      : null;
+
+    if(!savedFaveProductIds){
+      return false;
+    }
+
+    const updatedSavedFaves = savedFaveProductIds?.filter((savedFaveProductIds) => savedFaveProductIds !== productId );
+    localStorage.setItem('savedFave', JSON.stringify(updatedSavedFaves));
+
+    return true;
+  };
