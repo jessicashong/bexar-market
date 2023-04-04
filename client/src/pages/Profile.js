1020f34d0702477a7a8d4e9299233447733eb1f5
mdinkelbach <mj.dinkelbach@mgail.com>
2023-04-03 20:09:03 -0500

    merge

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Auth from "../utils/auth";
-import { Link, useParams } from "react-router-dom";
 import BusinessProductList from '../components/BusinessProductList';
 import UserFavoriteList from '../components/UserFavoriteList';
 import BusinessUpdateModal from "../components/BusinessUpdateModal";
@@ -8,7 +7,7 @@ import UserUpdateModal from '../components/UserUpdateModal';
 import { useQuery } from '@apollo/client';
 import { QUERY_ME, QUERY_BUSINESS } from '../utils/queries';
 
-function Profile({ isBusiness }) {
+function Profile() {
 
     const [showUserModal, setShowUserModal] = useState(false);
     const [showBusinessModal, setShowBusinessModal] = useState(false);
@@ -27,83 +26,6 @@ function Profile({ isBusiness }) {
     const userData = data?.me || [];
     console.log('userData:', userData);
 
-    // function showProfile() {
-    //     return 'Test';
-    //     if (token) {
-    //         if (userData.userName === undefined) {
-    //             return (
-    //                 <div>
-    //                     <h2>Hello</h2>
-    //                     {/*<BusinessProductList />*/}
-    //                     <div>
-    //                     <button
-    //                         type="button"
-    //                         className="bg-orange-400 text-center p-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
-    //                         onClick={ () => {
-    //                             setShowBusinessModal(true);
-    //                             }
-    //                         }
-    //                     >
-    //                         Click to Update Credentials
-    //                     </button>
-    //                     <BusinessUpdateModal
-    //                         isVisible={showBusinessModal}
-    //                         onClose={() => {
-    //                         setShowBusinessModal(false);
-    //                             }
-    //                         }
-    //                     />
-    //                     </div>
-    //                     <div>
-    //                         <button>Add Product</button>
-    //                     </div>
-    //                 </div>
-    //             )
-    //         } else {
-    //             return (
-    //                 <div className='user-profile'>
-    //                     <h2>Hello, {userData.userName}!</h2>
-    //                     <p>Favorites list coming soon!</p>
-    //                     <div>
-    //                     <button
-    //                         type="button"
-    //                         className="bg-orange-400 text-center p-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
-    //                         onClick={ () => {
-    //                             setShowUserModal(true);
-    //                             }
-    //                         }   
-    //                     >
-    //                         Click to Update Credentials
-    //                     </button>
-    //                     <UserUpdateModal
-    //                         isVisible={showUserModal}
-    //                         onClose={() => {
-    //                         setShowUserModal(false);
-    //                             }
-    //                         }
-    //                     />
-    //                     </div>
-    //                 </div>
-    //             )
-    //         }
-    //     } else {
-    //         return (
-    //             <div>
-    //                 <p>Please <Link to="/login">log in</Link> to view your profile</p>
-    //             </div>
-    //         )
-    //     }
-    // }
-    // { this.state.loadingPage
-    //     ? <span className="sr-only">Loading... Registered Devices</span>
-    //     : <>
-    //         {this.state.someBoolean
-    //           ? <div>some title</div>
-    //           : null
-    //         }
-    //         <div>body</div>
-    //       </>
-    //   }
     return loading ? <div>I am loading, yay</div> : <div>
         {
             bizId ? 
